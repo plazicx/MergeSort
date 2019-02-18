c39558305eed3b902c47f926175f65bd39d2c81b
Predrag Lazic <plazicx@gmail.com>
2019-02-19 00:20:23 +0100

    simple change

--- MergeSort/MergeSort.cpp
+++ MergeSort/MergeSort.cpp
@@ -204,7 +204,7 @@ int main()
 		std::cout << std::endl;
 	}
 }
-
+//simple change 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
